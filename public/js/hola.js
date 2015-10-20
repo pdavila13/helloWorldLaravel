6866a253d22e880cbb4b205856a0e664ddf36552
Paolo Davila <paolodavila@iesebre.com>
2015-10-20 21:51:27 +0200

    Example worked asincron

--- public/js/hola.js
+++ public/js/hola.js
@@ -1,1 +1,9 @@
-console.info("Hello World!");
+// Es defineix la funció
+function hello(){
+    console.info("Hello World!");
+}
+
+// Es defineix la funció
+function bye(){
+    console.info("Bye World!");
+}
